204984e34bed08888b5a0052c1052fbd5cc18108
Tushya-web <tushyparmar@gmail.com>
2025-10-04 19:16:49 +0530

    Update settings.py

--- attendease/settings.py
+++ attendease/settings.py
@@ -10,6 +10,7 @@ os.environ["DEEPFACE_HOME"] = os.path.join(BASE_DIR, "media", "deepface_models")
 
 import os
 
+ALLOWED_HOSTS = ['attend-ease-backend.onrender.com']
 SECRET_KEY = os.environ.get('SECRET_KEY', '97665b4de43d2bab61ba698612f18347')
 
 AUTH_USER_MODEL = "accounts.CustomUser"
