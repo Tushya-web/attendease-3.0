dc62e75e26167e830b6410bb4f4ae9e5667516f2
Tushya-web <tushyparmar@gmail.com>
2025-10-27 15:20:41 +0530

    chnage the localhost setting

--- attendease/settings.py
+++ attendease/settings.py
@@ -13,7 +13,7 @@ import os
 if os.environ.get('RENDER'):
     DEBUG = False
     
-ALLOWED_HOSTS = ['attendease-rl90.onrender.com']
+ALLOWED_HOSTS = ['attendease-rl90.onrender.com', 'localhost', '127.0.0.1']
 
 SECRET_KEY = os.environ.get('SECRET_KEY', '97665b4de43d2bab61ba698612f18347')
 
